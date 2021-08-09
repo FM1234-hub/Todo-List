361b3861e2ef94daf89f20e96e7dee4e16689e9f
cruzalain99 <ancel.cruz@ucsp.edu.pe>
2021-08-09 13:07:10 -0500

    import LoginView

--- base/views.py
+++ base/views.py
@@ -3,8 +3,10 @@ from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
-from .models import Task
 
+from django.contrib.auth.views import LoginView
+
+from .models import Task
 
 class TaskList(ListView):
     model = Task
