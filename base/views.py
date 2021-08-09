91446054ba3835f0060a22fe08ba6708bcaafe98
cruzalain99 <ancel.cruz@ucsp.edu.pe>
2021-08-09 13:11:24 -0500

    class CustomLoginView created

--- base/views.py
+++ base/views.py
@@ -6,6 +6,14 @@ from django.urls import reverse_lazy
 
 from django.contrib.auth.views import LoginView
 
+class CustomLoginView(LoginView):
+    template_name = 'base/login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('tasks')
+    
 from .models import Task
 
 class TaskList(ListView):
